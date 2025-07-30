7830f3dea84e5f13b067523d27aaa6f4d76cfe47
NeRrO106 <andreilimit@yahoo.com>
2025-07-30 15:52:49 +0300

    Product model

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -9,8 +9,8 @@ namespace QUickDish.API.Data
         {
         }
         public DbSet<User> Users { get; set; }
-        /*public DbSet<Product> Products { get; set; }
-        public DbSet<Order> Orders { get; set; }
+        public DbSet<Product> Products { get; set; }
+        /*public DbSet<Order> Orders { get; set; }
         public DbSet<Order_Item> Order_Items { get; set; }*/
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
--- Migrations/20250730125218_CreateProductTable.Designer.cs
+++ Migrations/20250730125218_CreateProductTable.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using QUickDish.API.Data;
 
@@ -10,9 +11,11 @@ using QUickDish.API.Data;
 namespace QUickDish.API.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250730125218_CreateProductTable")]
+    partial class CreateProductTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.7");
