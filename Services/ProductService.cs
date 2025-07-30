7ee3dec8bd40b0f9cbbc346934526d3df5d4aa98
NeRrO106 <andreilimit@yahoo.com>
2025-07-30 16:14:40 +0300

    Implementat ProductService

--- Services/ProductService.cs
+++ Services/ProductService.cs
@@ -1,6 +1,38 @@
-﻿namespace QUickDish.API.Services
+﻿using QUickDish.API.DTOs;
+using QUickDish.API.Models;
+using QUickDish.API.Repos;
+
+namespace QUickDish.API.Services
 {
     public class ProductService
     {
+        private readonly ProductRepo _productRepo;
+        public ProductService(ProductRepo productRepo)
+        {
+            _productRepo = productRepo;
+        }
+
+        public async Task<List<Product>> GetAllProductsAsync()
+        {
+            return await _productRepo.GetAllProductsAsync();
+        }
+
+        public async Task<Product?> GetProductByIdAsync(int id)
+        {
+            return await _productRepo.GetProductByIdAsync(id);
+        }
+
+        public async Task<Product> CreateProductAsync(CreateProductDto dto)
+        {
+            return await _productRepo.CreateProductAsync(dto);
+        }
+        public async Task<bool> DeleteProductAsync(int id)
+        {
+            return await _productRepo.DeleteProduct(id);
+        }
+        public async Task<bool> UpdateProductAsync(int id, CreateProductDto dto)
+        {
+            return await _productRepo.UpdateProductAsync(id, dto);
+        }
     }
 }
