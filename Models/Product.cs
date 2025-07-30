83696d8d284e50242b551a72d1203f525214c8f5
NeRrO106 <andreilimit@yahoo.com>
2025-07-30 16:12:26 +0300

    Implementat ProductRepo

--- Models/Product.cs
+++ Models/Product.cs
@@ -3,10 +3,10 @@
     public class Product
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public string Category { get; set; }
+        public required string Name { get; set; }
+        public required string Description { get; set; }
+        public required string Category { get; set; }
         public decimal Price { get; set; }
-        public string ImageUrl { get; set; }
+        public string? ImageUrl { get; set; }
     }
 }
---
+++ Repos/ProductRepo.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QUickDish.API.Data;
+using QUickDish.API.DTOs;
+using QUickDish.API.Models;
+
+namespace QUickDish.API.Repos
+{
+    public class ProductRepo
+    {
+        private readonly AppDbContext _context;
+
+        public ProductRepo(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Product>> GetAllProductsAsync()
+        {
+            return await _context.Products.ToListAsync();
+        }
+
+        public async Task<Product?> GetProductByIdAsync(int id)
+        {
+            return await _context.Products.FirstOrDefaultAsync(p => p.Id == id);
+        }
+
+        public async Task<Product> CreateProductAsync(CreateProductDto dto)
+        {
+            var product = new Product
+            {
+                Name = dto.Name,
+                Description = dto.Description,
+                Category = dto.Category,
+                Price = dto.Price,
+                ImageUrl = dto.ImageUrl
+            };
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+            return product;
+        }
+
+        public async Task<bool> DeleteProduct(int id)
+        {
+            var product = await _context.Products.FirstOrDefaultAsync(p => p.Id == id);
+
+            if (product == null)
+                return false;
+            _context.Products.Remove(product);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<bool> UpdateProductAsync(int id, CreateProductDto dto)
+        {
+            var product = await _context.Products.FirstOrDefaultAsync(p => p.Id == id);
+            if (product == null)
+                return false;
+            if (!string.IsNullOrEmpty(dto.Name))
+                product.Name = dto.Name;
+            if (!string.IsNullOrEmpty(dto.Description))
+                product.Description = dto.Description;
+            if (!string.IsNullOrEmpty(dto.Category))
+                product.Category = dto.Category;
+            if (dto.Price > 0)
+                product.Price = dto.Price;
+            if (!string.IsNullOrEmpty(dto.ImageUrl))
+                product.ImageUrl = dto.ImageUrl;
+            _context.Products.Update(product);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+    }
+}
