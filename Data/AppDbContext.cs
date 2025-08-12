6e79ea89fb2cc08b4199eeae2b560c22029050e8
NeRrO106 <andreilimit@yahoo.com>
2025-08-12 15:10:13 +0300

    Removed OrderItemResponse

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -5,9 +5,7 @@ namespace QUickDish.API.Data
 {
     public class AppDbContext : DbContext
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
-        {
-        }
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
         public DbSet<User> Users { get; set; }
         public DbSet<Product> Products { get; set; }
         public DbSet<Order> Orders { get; set; }
