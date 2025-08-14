aa5df51c3adbbd31f4ae599141102edf404c1c65
NeRrO106 <andreilimit@yahoo.com>
2025-08-14 19:11:38 +0300

    Secure endpoints

--- Startup.cs
+++ Startup.cs
@@ -2,6 +2,7 @@ using Microsoft.EntityFrameworkCore;
 using QUickDish.API.Data;
 using QUickDish.API.Repos;
 using QUickDish.API.Services;
+using System.Security.Claims;
 
 namespace QUickDish.API
 {
@@ -75,20 +76,36 @@ namespace QUickDish.API
                     options.ExpireTimeSpan = TimeSpan.FromHours(2);
                     options.SlidingExpiration = true;
                     options.Cookie.HttpOnly = false;
+                    options.Cookie.SameSite = SameSiteMode.None;
+                    options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
                 });
 
             services.AddAuthorization(options =>
             {
-                options.AddPolicy("RequireAdminRole", policy => policy.RequireRole("Admin"));
-                options.AddPolicy("RequireUserRole", policy => policy.RequireRole("User"));
-                options.AddPolicy("RequireCourierRole", policy => policy.RequireRole("Courier"));
-                options.AddPolicy("RequiredManagerRole", policy => policy.RequireRole("Manager"));
-                options.AddPolicy("RequiredAdminOrManagerOrCourierRole", policy =>
-                    policy.RequireRole("Admin", "Manager", "Courier"));
-                options.AddPolicy("RequiredAdminOrManagerRole", policy =>
-                    policy.RequireRole("Admin", "Manager"));
-            });
+                options.AddPolicy("RequireAdminRole", policy => policy.RequireClaim(ClaimTypes.Role, "Admin"));
+                options.AddPolicy("RequireUserRole", policy => policy.RequireClaim(ClaimTypes.Role, "Client"));
+                options.AddPolicy("RequireCourierRole", policy => policy.RequireClaim(ClaimTypes.Role, "Courier"));
+                options.AddPolicy("RequiredManagerRole", policy => policy.RequireClaim(ClaimTypes.Role, "Manager"));
 
+                options.AddPolicy("RequiredAdminOrManagerOrCourierRole", policy =>
+                {
+                    policy.RequireAssertion(context =>
+                        context.User.HasClaim(c => c.Type == ClaimTypes.Role &&
+                                                    (c.Value == "Admin" ||
+                                                     c.Value == "Manager" ||
+                                                     c.Value == "Courier"))
+                    );
+                });
+
+                options.AddPolicy("RequiredAdminOrManagerRole", policy =>
+                {
+                    policy.RequireAssertion(context =>
+                        context.User.HasClaim(c => c.Type == ClaimTypes.Role &&
+                                                    (c.Value == "Admin" ||
+                                                     c.Value == "Manager"))
+                    );
+                });
+            });
             services.AddControllers()
                 .AddJsonOptions(options =>
                 {
